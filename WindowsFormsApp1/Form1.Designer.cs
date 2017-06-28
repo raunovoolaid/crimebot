5e38dc780aa9fcd0948ea00fd4830dc7e1dce2ef
HeikoParmas <hparmas@gmail.com>
2017-06-29 01:03:13 +0300

    Records Timer More specific error messages Future stats kind of UI

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,16 +30,30 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.geckoWebBrowser1 = new Gecko.GeckoWebBrowser();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(906, 77);
+            this.button1.Location = new System.Drawing.Point(923, 196);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "START";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -52,23 +66,165 @@
             this.geckoWebBrowser1.TabIndex = 1;
             this.geckoWebBrowser1.UseHttpActivityObserver = false;
             // 
-            // checkBox1
+            // label1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(925, 142);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(941, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(941, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(941, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(941, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1028, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1028, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1028, 56);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1028, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "JOOGIMEISTER",
+            "KÄSITÖÖ",
+            "KEEMIK"});
+            this.listBox1.Location = new System.Drawing.Point(943, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1004, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(906, 233);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Edukaid klikke järjest: 0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(971, 257);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(33, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "käike";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1048, 233);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Käidud aeg";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(906, 257);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(50, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Rekordid";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(1048, 257);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(25, 13);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "aeg";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1016, 667);
-            this.Controls.Add(this.checkBox1);
+            this.ClientSize = new System.Drawing.Size(1141, 667);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.geckoWebBrowser1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -82,7 +238,21 @@
 
         private System.Windows.Forms.Button button1;
         private Gecko.GeckoWebBrowser geckoWebBrowser1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using System.Diagnostics;
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        
+        Stopwatch watch = new Stopwatch();
         async Task PutTaskDelay()
         {
             Random rnd = new Random();
@@ -22,63 +24,183 @@ namespace WindowsFormsApp1
 
         public Form1()
         {
+            //createTimer();
             InitializeComponent();
+            label1.Text = "Joogimeistri level:";
+            label2.Text = "Käsitöö level:";
+            label3.Text = "Keemiku level:";
             geckoWebBrowser1.Navigate("http://www.crime.ee");
+            
             //test
         }
+        public static class Globals
+        {
+            public static int rekordKlikke = 0;
+            public static int kasClickida = 0;
+            public static string rekordAeg = "0";
+        }
+
         public void make ()
         {
-            
+            label11.Text = watch.Elapsed.TotalSeconds.ToString() + "s";
+            if(double.Parse(watch.Elapsed.TotalSeconds.ToString()) > double.Parse(Globals.rekordAeg))
+            {
+                Globals.rekordAeg = watch.Elapsed.TotalSeconds.ToString();
+                label13.Text = Globals.rekordAeg + "s";
+            }
             Gecko.GeckoElement btn = (Gecko.GeckoElement)geckoWebBrowser1.DomDocument.GetElementsByClassName("nupuke420")[0];
             Gecko.GeckoHtmlElement button = (Gecko.GeckoHtmlElement)btn;
             button.Click();
             return;
         }
-        private void button1_Click(object sender, EventArgs e)
-        {   
-             if (captchaCheck())
-             {
-                 make();
-             }
-             else
-                 System.Media.SystemSounds.Asterisk.Play();
+        private async void button1_Click(object sender, EventArgs e)
+        {
             
+            
+        label9.Text = "Edukaid klikke järjest: 0";
+            //Stopwatch stopper = new Stopwatch();
+            watch.Reset();
+            watch.Start();
+            Globals.kasClickida = 1;
+            int counter = 0;
+            do
+            {
+                make();
+                counter++;
+                if (Globals.rekordKlikke < counter)
+                {
+                    Globals.rekordKlikke = counter;
+                    label10.Text = "Rekord: "+counter;
+                }
+                label9.Text = "Edukaid klikke järjest: " + counter;
+                await PutTaskDelay();
+                getStats();
+
+            }
+            while (captchaCheck() && Globals.kasClickida == 1);
         }
 
         public bool captchaCheck()
         {
             Gecko.GeckoHtmlElement captcha = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("captcha_container");
+            Gecko.GeckoNodeCollection veaSisud = (Gecko.GeckoNodeCollection)geckoWebBrowser1.DomDocument.GetElementsByClassName("message info");
+            int veaCount = 0;
+            Gecko.GeckoHtmlElement vigaSisu = null;
+            foreach (Gecko.GeckoNode veaSisu in veaSisud)
+            {
+                veaCount++;
+                if(veaCount > 1)
+                {
+                   vigaSisu = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementsByClassName("message info")[1];
+                    break;
+                }
+            }
+
             if (captcha == null)
             {
-                Console.WriteLine("caphcha not found");
-                return false;
+                
+                if (vigaSisu == null)
+                {
+                    Console.WriteLine("caphcha not found and viga is null");
+                    return true;
+                }
+                else
+                {
+                    if (vigaSisu.InnerHtml.Contains("tellida"))
+                    {
+                        Console.WriteLine("tellida aineid");
+                        Globals.kasClickida = 0;
+                        System.Media.SystemSounds.Asterisk.Play();
+                        watch.Stop();
+                        return false;
+                    }
+                    else
+                    {
+                        if(vigaSisu.InnerHtml.Contains("valmistada")){
+                            Console.WriteLine("valmistada mahla");
+                            Globals.kasClickida = 0;
+                            System.Media.SystemSounds.Asterisk.Play();
+                            watch.Stop();
+                            return false;
+                        }
+                        else
+                        {
+                            Console.WriteLine("caphcha hidden and viga is OK");
+                            return true;
+                        }
+                    }
+                }
             }
             else
             {
                 if (captcha.GetAttribute("style").Contains("display: none"))
                 {
-                    Console.WriteLine("captcha peidus");
-                    return true;
+                        if (vigaSisu == null)
+                        {
+                            Console.WriteLine("caphcha hidden and viga is null");
+                            return true;
+                        }
+                        else
+                        {
+                            if (vigaSisu.InnerHtml.Contains("tellida"))
+                            {
+                                Console.WriteLine("caphcha hidden and tellida aineid");
+                                Globals.kasClickida = 0;
+                                System.Media.SystemSounds.Asterisk.Play();
+                                watch.Stop();
+                                return false;
+                            }
+                            else
+                            {
+                                if (vigaSisu.InnerHtml.Contains("valmistada"))
+                                {
+                                    Console.WriteLine("caphcha hidden and valmistada mahla");
+                                    Globals.kasClickida = 0;
+                                    System.Media.SystemSounds.Asterisk.Play();
+                                    watch.Stop();
+                                    return false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("caphcha hidden and viga is OK");
+                                    return true;
+                                }
+                            }
+                        }
                 }
-                else
+                else //(!captcha.GetAttribute("style").Contains("display: none"))
                 {
                     Console.WriteLine("captcha detected");
-                    checkBox1.Checked = false;
+                    Globals.kasClickida = 0;
                     System.Media.SystemSounds.Asterisk.Play();
+                    watch.Stop();
                     return false;
                 }
             }
+            /* igaks juhuks saved
+                }
+                Console.WriteLine("captcha detected");
+                Globals.kasClickida = 0;
+                System.Media.SystemSounds.Asterisk.Play();
+                MessageBox.Show(vigaSisu. + "FIX IT");
+                return false;*/
         }
-
-        private async void checkBox1_CheckedChanged(object sender, EventArgs e)
+        public void getStats()
         {
-            do
-            {
-                make();
-                await PutTaskDelay();
-
-            }
-            while (checkBox1.Checked && captchaCheck());
+            Gecko.GeckoHtmlElement kasitoolvl = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("s_crafting");
+            Gecko.GeckoHtmlElement keemiklvl = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("s_chemistry");
+            Gecko.GeckoHtmlElement joogilvl = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("s_barkeeping");
+            label5.Text = joogilvl.InnerHtml;
+            label6.Text = kasitoolvl.InnerHtml;
+            label7.Text = keemiklvl.InnerHtml;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Globals.kasClickida = 0;
+            watch.Stop();
+        }
+
+
     }
 }
