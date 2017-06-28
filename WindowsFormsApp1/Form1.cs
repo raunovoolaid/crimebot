65e45f70ab48171cc7644aae82bf5a921d33dbdc
Rauno Voolaid <rauno.voolaid@windowslive.com>
2017-06-28 21:41:31 +0300

    working bot

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -22,7 +23,7 @@ namespace WindowsFormsApp1
             Gecko.GeckoHtmlElement Btn = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("nupuke420_aerator");
             Btn.Click();
         }
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             
             Gecko.GeckoHtmlElement Moves = ((Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("moves_left"));
@@ -30,31 +31,24 @@ namespace WindowsFormsApp1
             Console.WriteLine(Moves.ToString());
             Console.WriteLine(Move);
             
-            Console.WriteLine(Move);
-            int i = 0;
-            do
+            
+            for (int i = 0; i < int.Parse(Move); i++)
             {
-                Gecko.GeckoHtmlElement Btn = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("nupuke420_aerator");
-                Btn.Click();
-                wait(900);
-                i++;
+                make();
+                await PutTaskDelay();
+                Console.WriteLine(Move);
             }
-            while(i < 3);
-        }
-        void wait(int x)
-        {
-            DateTime t = DateTime.Now;
-            DateTime tf = DateTime.Now.AddMilliseconds(x);
 
-            while (t < tf)
-            {
-                t = DateTime.Now;
-            }
         }
+        
 
         private void geckoWebBrowser1_Click(object sender, EventArgs e)
         {
 
+        }
+        async Task PutTaskDelay()
+        {
+            await Task.Delay(600);
         }
     }
 }
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -20,3 +20,4 @@ namespace WindowsFormsApp1
         }
     }
 }
+
