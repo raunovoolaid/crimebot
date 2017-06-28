07c65099dcec4446e1d06a542be858774ae3ddb2
xheiksx <heiks@live.com>
2017-06-28 22:07:49 +0300

    fafafeef

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,40 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        
+        async Task PutTaskDelay()
+        {
+            await Task.Delay(600);
+        }
+
         public Form1()
         {
             InitializeComponent();
             geckoWebBrowser1.Navigate("http://www.crime.ee");
             //test
-
         }
         public void make ()
         {
             
-            Gecko.GeckoHtmlElement Btn = (Gecko.GeckoHtmlElement)geckoWebBrowser1.DomDocument.GetElementById("nupuke420_aerator");
-            Btn.Click();
+            Console.WriteLine("test");
+            Gecko.GeckoElement btn = (Gecko.GeckoElement)geckoWebBrowser1.DomDocument.GetElementsByClassName("nupuke420")[0];
+            Gecko.GeckoHtmlElement button = (Gecko.GeckoHtmlElement)btn;
+            button.Click();
+            return;
         }
         private void button1_Click(object sender, EventArgs e)
         {   
              if (captchaCheck())
              {
-                 click();
+                 make();
              }
              else
                  System.Media.SystemSounds.Asterisk.Play();
             
-        }
-        void click()
-        {
-            Gecko.GeckoElement btn = (Gecko.GeckoElement)geckoWebBrowser1.DomDocument.GetElementsByClassName("nupuke420")[0];
-            Gecko.GeckoHtmlElement button = (Gecko.GeckoHtmlElement)btn;
-            button.Click();
-            return;
         }
         void wait(int x)
         {
@@ -75,12 +76,13 @@ namespace WindowsFormsApp1
             }
         }
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        private async void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            for (var i = 0; i < 1; i++)
+            for (var i = 0; i < 3; i++)
             {
-                button1_Click(sender, e);
-                wait(300);
+                make();
+                await PutTaskDelay();
+                
             }
         }
     }
